4ecb10d71f5761a5963d8492821bc529e0e0da96
Jacob Begin <jacob.begin@jbegin-mac.bit9.local>
2017-03-07 19:13:06 -0500

    Adding packaging files

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.dirname(__file__)
+
+setup(
+    name='razor',
+    version='0.0.1',
+    author='Jacob Begin',
+    author_email='jebeginmail@gmail.com',
+    description='POC of vagrant python3',
+    packages=find_packages(),
+    # scripts=[os.path.join(here, "razor/foo.py")],
+    install_requires=[
+        'python-vagrant>=0.5.14'
+    ],
+    include_package_data=True)
